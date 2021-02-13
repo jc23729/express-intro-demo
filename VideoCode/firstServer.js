0fcd917920a1d6e6260f756432eb56285ac652d7
Juan Cruz <jc23729@gmail.com>
2021-02-13 18:22:41 -0500

    Update firstServer.js
    setup a port 3000 similar to boilerplate for flask

--- VideoCode/firstServer.js
+++ VideoCode/firstServer.js
@@ -2,7 +2,7 @@
 const express = require('express');
 
 const app = express();
-
+//app.listen should always be at the bottom of the file 
 app.listen(3000, () => {
   console.log("Server running on port 3000")
 });
